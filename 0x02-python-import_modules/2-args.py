59ffbb17d3b6d0b1be57f24ec4ac9fe86462bd1a
Minassie <minassieasfaw001@gmail.com>
2022-08-25 16:13:28 -0700

    fixed style err

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from sys import argv
     k = len(argv)
     print("{:d} {:s}{:s}".format(k - 1, "argument" if k <= 2 else "arguments",
-                                 "." if l == 1 else ":"))
+                                 "." if k == 1 else ":"))
     for i, s in enumerate(argv):
         if i > 0:
             print("{:d}: {:s}".format(i, s))
