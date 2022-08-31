8757ea0d450d3286dc298d2478ff1c9afe41f3fa
Minassie <minassieasfaw001@gmail.com>
2022-08-31 16:20:50 -0700

    more data structures commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,7 +6,7 @@ def roman_to_int(roman_string):
 
     roman_dict = {
             "I": 1,
-            "V":5,
+            "V": 5,
             "X": 10,
             "L": 50,
             "C": 100,
@@ -21,7 +21,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
